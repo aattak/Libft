e7d8f72ff3e7ba109c48529c296a0df7a686f36a
aattak <aattak@e2r13p7.1337.ma>
2023-11-18 11:59:09 +0100

    reproduce memmove function

---
+++ ft_memmove.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aattak <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 17:19:26 by aattak            #+#    #+#             */
+/*   Updated: 2023/11/18 11:16:25 by aattak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef>
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t	i;
+
+	if (dst < src)
+		return (ft_memcpy(dst, src, len));
+	i = len;
+	while (i > 0)
+	{
+		dst[i] = src[i];
+		i--;
+	}
+	return (dst);
+}
