128bc05474ccec0852a2c653f5cab379a80b373a
aattak <aattak@e2r13p7.1337.ma>
2023-11-22 07:01:17 +0100

    backup ft_split before breakfast

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: aattak <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 15:28:23 by aattak            #+#    #+#             */
-/*   Updated: 2023/11/21 16:28:58 by aattak           ###   ########.fr       */
+/*   Updated: 2023/11/22 07:00:19 by aattak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,53 @@
 
 static int	ft_count_words(char const *s, char c)
 {
+	int	i;
+	int	l;
+
+	i = 0;
+	if (s[i] == '\0')
+		return (0);
+	l = 0;
+	if (s[i] != c)
+		l++;
+	i++;
+	while (s[i] != '\0')
+	{
+	}
+	return (l);
 }
 
-static char	*malloc_nd_fill()
+static int	word_len()
 {
 }
 
-static void	ft_free()
+static char	*malloc_nd_fill(char *s)
 {
+	char	*split;
+	return (split);
+}
+
+static void	ft_free(char **split, int i)
+{
+	while (i > 0)
+		free(split[i]);
+	free(split);
 }
 
 char	**ft_split(char const *s, char c)
 {
+	int		n_words;
 	char	**split;
 
-	split = (char **)malloc((ft_count_words(s, c) + 1) * sizeof(char *));
+	n_words = ft_count_words(s, c);
+	split = (char **)malloc((n_words + 1) * sizeof(char *));
 	if (split == NULL)
 		return (NULL);
-	while ()
+	while (i < n_words)
 	{
 		split[i] = malloc_nd_fill();
 		if (split[i] == NULL)
-			ft_free();
+			ft_free(split, i - 1);
 	}
 	return(split);
 }
