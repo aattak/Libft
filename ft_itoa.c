b21591152145145c472cf0bd15dc54623e369e67
aattak <aattak@e2r8p12.1337.ma>
2023-11-23 17:53:36 +0100

    create ft_itoa function

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: aattak <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 09:24:15 by aattak            #+#    #+#             */
-/*   Updated: 2023/11/23 14:17:49 by aattak           ###   ########.fr       */
+/*   Updated: 2023/11/23 17:52:45 by aattak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ static size_t	ilen(int n)
 		n *= -1;
 		len = 1;
 	}
-	while(n > 0)
+	while (n > 0)
 	{
 		n /= 10;
 		len++;
@@ -32,6 +32,13 @@ static size_t	ilen(int n)
 
 static char	*ft_fill(char *str, size_t len, int n)
 {
+	while (n > 0)
+	{
+		str[len] = (n % 10) + 48;
+		n /= 10;
+		len--;
+	}
+	return (str);
 }
 
 char	*ft_itoa(int n)
@@ -42,15 +49,22 @@ char	*ft_itoa(int n)
 	if (n == 0)
 		return (ft_strdup("0"));
 	if (n == -2147483648)
-		return (ft_strdup("-2147483648");
+		return (ft_strdup("-2147483648"));
 	len = ilen(n);
 	str = (char *)malloc(len + 1);
 	if (str == NULL)
 		return (NULL);
 	str[len] = '\0';
+	len--;
 	if (n < 0)
+	{
+		n *= -1;
+		ft_fill(&str[1], len - 1, n);
 		str[0] = '-';
-
-
+	}
+	else
+	{
+		ft_fill(str, len, n);
+	}
 	return (str);
 }
