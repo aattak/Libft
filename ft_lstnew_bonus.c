1b060500cda0249b4b237456e6e85c710c0f961b
aattak <aattak@e2r13p7.1337.ma>
2023-11-30 18:33:59 +0100

    create ft_lstnew bonus function

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aattak <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 17:59:39 by aattak            #+#    #+#             */
+/*   Updated: 2023/11/30 18:27:45 by aattak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*nnode;
+
+	nnode = (t_list *)malloc(sizeof(t_list));
+	if (!nnode)
+		return (NULL);
+	nnode->content = content;
+	nnode->next = NULL;
+	return (nnode);
+}
