d4db5f53011bb73ba1795995d2e6404c8b55d50c
aattak <aattak@e2r13p7.1337.ma>
2023-12-01 15:47:43 +0100

    create ft_lstadd_back bonus function

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aattak <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/01 12:30:07 by aattak            #+#    #+#             */
+/*   Updated: 2023/12/01 15:30:20 by aattak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!lst || !new)
+		return ;
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
+	new->next = NULL;
+}
