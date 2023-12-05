fac10e1502f6f6e791d0a4a59ea79406a7e18203
aattak <aattak@e2r13p7.1337.ma>
2023-12-05 14:51:05 +0100

    create ft_lstmap bonus function

---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aattak <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 19:11:39 by aattak            #+#    #+#             */
+/*   Updated: 2023/12/05 14:42:28 by aattak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*rlst;
+	t_list	*new;
+	void	*tmp;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	rlst = NULL;
+	while (lst)
+	{
+		tmp = f(lst->content);
+		new = ft_lstnew(tmp);
+		if (!new)
+		{
+			del(tmp);
+			ft_lstclear(&rlst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&rlst, new);
+		lst = lst->next;
+	}
+	return (rlst);
+}
