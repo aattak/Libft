02f342e0bfbe81237a677fc4a06758e93bfd3554
aattak <aattak@e2r13p7.1337.ma>
2023-12-07 18:25:50 +0100

    reproduce strnstr function

---
+++ ft_strnstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aattak <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 21:39:42 by aattak            #+#    #+#             */
+/*   Updated: 2023/12/07 18:22:06 by aattak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!(ft_strlen(needle)))
+		return ((char *)haystack);
+	i = 0;
+	while (haystack[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (needle[j] && haystack[i + j] && i < len - j
+			&& needle[j] == haystack[i + j])
+			j++;
+		if (needle[j] == '\0')
+			return ((char *)haystack + i);
+		i++;
+	}
+	return (NULL);
+}
