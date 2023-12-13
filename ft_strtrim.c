ae1dd59a96386ba3b33f31be3be6e95f57936594
aattak <aattak@e2r13p9.1337.ma>
2023-12-13 10:00:21 +0100

    fix ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: aattak <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 08:49:11 by aattak            #+#    #+#             */
-/*   Updated: 2023/12/12 22:07:45 by aattak           ###   ########.fr       */
+/*   Updated: 2023/12/13 09:57:24 by aattak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	end;
 	char	*trimmed;
 
-	if (!s || !set)
+	if (!s1 || !set)
 		return (NULL);
 	slen = ft_strlen(s1);
 	start = 0;
